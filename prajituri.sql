df343a5247a7c65c0805995761aeee3cb9641005
StoicaIoan <stoicaioan2002@gmail.com>
2022-03-28 17:48:18 +0300

    Baza de date

--- prajituri.sql
+++ prajituri.sql
@@ -1,8 +1,8 @@
-DROP TYPE IF EXISTS categ_prajitura;
+DROP TYPE IF EXISTS categ_pc;
 DROP TYPE IF EXISTS tipuri_produse;
 
-CREATE TYPE categ_prajitura AS ENUM( 'comanda speciala', 'aniversara', 'editie limitata', 'pentru copii', 'dietetica','comuna');
-CREATE TYPE tipuri_produse AS ENUM('cofetarie', 'patiserie', 'gelaterie');
+CREATE TYPE categ_pc AS ENUM( 'CPU', 'RAM', 'GPU');
+CREATE TYPE tipuri_piesa AS ENUM('rapida', 'obisnuita', 'lenta');
 
 
 CREATE TABLE IF NOT EXISTS prajituri (
@@ -10,12 +10,11 @@ CREATE TABLE IF NOT EXISTS prajituri (
    nume VARCHAR(50) UNIQUE NOT NULL,
    descriere TEXT,
    pret NUMERIC(8,2) NOT NULL,
-   gramaj INT NOT NULL CHECK (gramaj>=0),   
-   tip_produs tipuri_produse DEFAULT 'cofetarie',
-   calorii INT NOT NULL CHECK (calorii>=0),
-   categorie categ_prajitura DEFAULT 'comuna',
-   ingrediente VARCHAR [], --pot sa nu fie specificare deci nu punem NOT NULL
-   pt_diabetici BOOLEAN NOT NULL DEFAULT FALSE,
+   an_fabricatie INT NOT NULL CHECK ('2022') ,
+   greutate INT NOT NULL CHECK (greutate>=0),
+   categorie categ_pc DEFAULT 'piesa pc',
+   marimi VARCHAR [], --pot sa nu fie specificare deci nu punem NOT NULL
+   curier BOOLEAN NOT NULL DEFAULT FALSE,
    imagine VARCHAR(300),
    data_adaugare TIMESTAMP DEFAULT current_timestamp
 );
