8be4dd7fc2813b6037a9e21c6725b24ef7827e0d
Ioan <ioan.stoica@kodingtech.com>
2022-08-16 11:14:46 +0300

    task 6

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
 const express = require('express')
+const path = require('path');
+
 const app = express()
 const port = 8080
 
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
 app.use(express.static('resurse'))
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+// index page
+app.get('/', function(req, res) {
+  res.render('index');
+});
+
+app.get('/home', function(req, res) {
+  res.render('index');
 })
 
+app.get('/index', function(req, res) {
+  res.render('index');
+})
+
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port} and serving path ${path.join(__dirname, '/index.html')}`)
 })
